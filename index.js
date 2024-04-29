a794bad25b0706287473405cb0b03663e8aedc89
Daniel <dani.v2509@outlook.com>
2024-04-29 11:56:01 +0200

    Nickname support implemented

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import chalk from 'chalk';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
-import chalk from 'chalk';
-import { error } from 'node:console';
+import { error, log } from 'node:console';
 import { availableParallelism } from 'node:os';
 import cluster from 'node:cluster';
 import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
+
+
 
 if (cluster.isPrimary) {
     const numCPUs = availableParallelism();
@@ -39,78 +41,125 @@ if (cluster.isPrimary) {
 
     });
 
+    const users = {}
 
-    // Use or create DB
-    const db = await open({
+     // Use or create DB
+     const db = await open({
         filename: 'chat.db',
         driver: sqlite3.Database
-    });
+        });
+    
+        // Create messages table
+        await db.exec(`
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nickname TEXT,
+                clientOffset TEXT UNIQUE,
+                content TEXT
+            );
+        `);
+        console.log(chalk.green('Messages database initiated correctly!'));
 
-    // Create messages table
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS messages (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            content TEXT
-        );
-    `);
-    // console.log(chalk.green('Messages database initiated correctly!'));
-
+    // Send our 'index.html' to the user
     const __dirname = dirname(fileURLToPath(import.meta.url));
-
     app.get('/', (req, res) => {
         res.sendFile(join(__dirname, 'index.html'));
     });
 
 
-
+    // When a new socket connects
     io.on('connection', async (socket) => {
-        console.log('a user connected - online');
-        socket.emit('userJoined', 'A user has joined the chat');
+        console.log('New user connected:', chalk.gray(`${socket.id}`));
+        // socket.emit('userJoined', `New user connected (${socket.id})`);
+
+        // ! ==================================================================================
+
+            // Debug counter
+            let counter = 0;
+
+        // ! ==================================================================================
+
+        socket.on('setNickname', (nickname, callback) => {
+            let bool = true;
+            const recipient = socket.id;
+            if (nickname && !users[nickname]) {
+                users[nickname] = socket.id;
+                socket.nickname = nickname;
+                console.log(`${nickname} joined the chat`);
+                io.emit('message', `${nickname} joined the chat`);
+                socket.emit('isNicknameValid', { recipient, bool });
+
+                // Acknowledge the event: prevent from retries (error)
+                callback();
+
+                // Debug ===================================================
+
+                console.log(users);
+
+                // ==========================================================
+            } else {
+                // Inform user that the nickname is already in use
+                console.log('Repeated nickname', socket.id);
+                bool = false
+                
+                console.log(recipient, bool);
+
+                socket.emit('isNicknameValid', { recipient, bool });
+
+                // Acknowledge the event: prevent from retries (error)
+                callback();
+            }
+        });
         
 
-        const room = 'Room 1'
-        socket.join(room);
-        console.log('Joining room:', room);
+        //TODO Fordward him to a room
+        // const room = 'Room 1'
+        // socket.join(room);
+        // console.log('Joining room:', room);
 
-        // With broadcast, sends to everyone but the sender
+        //* Broadcast: sends to everyone but the sender
         socket.broadcast.emit('welcomeMsg', 'Hello!')
+
+        socket.on('set username', async (name) => {
+            console.log(socket);
+            console.log(name);
+        })
 
         // When a user sends a message
         socket.on('chat message', async (msg, clientOffset, callback) => {
-            // Insert the msg into the DB
+                // Insert the msg into the DB
             let result;
             try {
-                result = await db.run('INSERT INTO messages (content) VALUES (?)', msg, clientOffset);
+                //! Strange functioning of client offsets
+                result = await db.run('INSERT INTO messages (nickname, content) VALUES (?, ?)', [socket.nickname || 'User', msg]);
             }catch (e) {
                 if(e.errno === 19 /* SQLITE Error */){
                 // Message was already inserted
                 console.log('Message already inserted');
 
-                // Acknowledge the event - prevent from retries
+                // Acknowledge the event: prevent from retries (error)
                 callback();
                 } else {
+                    console.log(e);
                     console.log(e.message);
                     console.log('Retrying insertion...');
                 }
-                // TODO handle freature
             }
+            // Send message to every connected socket (including sender, for simplicity)
+            io.emit('chat message', socket.nickname, msg, result.lastID);        
 
-            // Send to every connected socket (including sender
-            // Done in this way for simplicity
-            io.emit('chat message', msg, result.lastID);        
-
-            // Acknowledge the event - prevent from retries if it was succesful
+            // Acknowledge the event: prevent from retries (success)
             callback();
-
         });
 
+        // if recovery state (of const 'io') didn't work 
         if (!socket.recovered) {
-            // if recovery state didn't work
             try {
-                await db.each('SELECT id, content FROM messages WHERE id > ?', 
+                // Update the chat with the messages not recieved while disconnected
+                await db.each('SELECT id, nickname, content FROM messages WHERE id > ?', 
                 [socket.handshake.auth.serverOffset || 0],
                 (_err, row) => {
-                    socket.emit('chat message', row.content, row.id);
+                    socket.emit('chat message', row.nickname, row.content, row.id);
                 })
             } catch (e) {
                 console.log('Recovery went wrong:', error);
